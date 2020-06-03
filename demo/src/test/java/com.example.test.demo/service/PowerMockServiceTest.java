244d6c4809367a74b99b0b3a7d59efd69c95ef8e
sandeep <sandeepkjl143@gmail.com>
2020-06-03 21:06:03 +0530

    adding one more test3 to powermock

--- demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
+++ demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
@@ -62,4 +62,19 @@ public class PowerMockServiceTest {
         System.out.println(service.getMultiply(7));
     }
 
+    @Test
+    public void getMultiplyTest3() throws IOException {
+
+
+        Mockito.when(helper.getFirstNumber()).thenReturn(8);
+
+        PowerMockito.mockStatic(PowerMockDao.class);
+
+        Mockito.when(PowerMockDao.multiply(8,9)).thenReturn(32);
+
+
+
+        System.out.println(service.getMultiply(7));
+    }
+
 }
