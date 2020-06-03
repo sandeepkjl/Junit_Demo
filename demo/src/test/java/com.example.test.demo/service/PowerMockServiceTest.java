733e8378d46e048e5125f77b266351904c2b5d83
sandeep <sandeepkjl143@gmail.com>
2020-06-03 22:31:33 +0530

    add test8 for powermock

--- demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
+++ demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
@@ -137,4 +137,19 @@ public class PowerMockServiceTest {
         System.out.println(service.getMultiply(7));
     }
 
+    @Test
+    public void getMultiplyTest8() throws IOException {
+
+
+        Mockito.when(helper.getFirstNumber()).thenReturn(8);
+
+        PowerMockito.mockStatic(PowerMockDao.class);
+
+        Mockito.when(PowerMockDao.multiply(8,9)).thenReturn(32);
+
+
+
+        System.out.println(service.getMultiply(7));
+    }
+
 }
