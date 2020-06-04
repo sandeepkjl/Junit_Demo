27b0af958d2e680bae2bcab0dd60c660547eaa8a
sandeepkjl <31666072+sandeepkjl@users.noreply.github.com>
2020-06-04 12:45:32 +0530

    add test 13 power mock

--- demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
+++ demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
@@ -213,5 +213,20 @@ public class PowerMockServiceTest {
 
         System.out.println(service.getMultiply(7));
     }
+    
+    @Test
+    public void getMultiplyTest13() throws IOException {
+
+
+        Mockito.when(helper.getFirstNumber()).thenReturn(8);
+
+        PowerMockito.mockStatic(PowerMockDao.class);
+
+        Mockito.when(PowerMockDao.multiply(8,9)).thenReturn(32);
+
+
+
+        System.out.println(service.getMultiply(7));
+    }
 
 }
