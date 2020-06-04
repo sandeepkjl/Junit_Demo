3597a81c375ba945f519aa7bc0ff9cdd03d5c37e
sandeep <sandeepkjl143@gmail.com>
2020-06-04 12:29:08 +0530

    adding test10 for junit4test

--- demo/src/test/java/com.example.test.demo/service/Junit4ServiceTest.java
+++ demo/src/test/java/com.example.test.demo/service/Junit4ServiceTest.java
@@ -94,6 +94,14 @@ public class Junit4ServiceTest {
 
     }
 
+    @Test
+    public void getSubTest10(){
+
+        Mockito.when(helper.sub(10,2)).thenReturn(5);
+        int sub=service.getSub(10,2);
+
+    }
+
 
 
 }
