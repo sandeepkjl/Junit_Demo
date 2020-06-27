05c413158df82ea6fa022500906e3af72b20c7b4
sandeep <sandeepkjl143@gmail.com>
2020-06-27 23:23:08 +0530

    change powerMockito getTest14

--- demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
+++ demo/src/test/java/com.example.test.demo/service/PowerMockServiceTest.java
@@ -229,4 +229,19 @@ public class PowerMockServiceTest {
         System.out.println(service.getMultiply(7));
     }
 
+    @Test
+    public void getMultiplyTest14() throws IOException {
+
+
+        Mockito.when(helper.getFirstNumber()).thenReturn(8);
+
+        PowerMockito.mockStatic(PowerMockDao.class);
+
+        Mockito.when(PowerMockDao.multiply(8,9)).thenReturn(32);
+
+
+
+        System.out.println(service.getMultiply(7));
+    }
+
 }
